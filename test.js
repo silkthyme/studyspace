96528d8e149bb3f448c47acbcffcd771feba005f
christina <christinahuangji@gmail.com>
2020-01-18 17:25:54 -0800

    added square feet of the arc

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var arc_wifi_key = 'https://ucd-pi-iis.ou.ad3.ucdavis.edu/piwebapi/streams/F1AbEbgZy4oKQ9kiBiZJTW7eugw1d09nFPu5hGUtUhRt5d2AA4adEPLuHRFMQvB3Pt0VKPgVVRJTC1BRlxBQ0VcVUMgREFWSVNcSUNTIEJVSUxESU5HU1xBUkN8V0lGSSBPQ0NVUEFOVFM/value';
-
+var arc_sq_ft_key = 'https://ucd-pi-iis.ou.ad3.ucdavis.edu/piwebapi/streams/F1AbEbgZy4oKQ9kiBiZJTW7eugwr1rAt6dQ5BG4fRgDcyrprwmFWwiEf1tVQseNk58UYmagVVRJTC1BRlxDRUZTXFVDREFWSVNcQlVJTERJTkdTXEFDVElWSVRJRVMgQU5EIFJFQ1JFQVRJT04gQ0VOVEVSXFNURUFNfFRPVEFMIE1BSU5UQUlORUQgR1JPU1MgU1EuIEZULg/value';
 fetch(arc_wifi_key)
   .then((response) => {
     return response.json();
@@ -8,3 +8,13 @@ fetch(arc_wifi_key)
     console.log(myJson['Value']);
     document.getElementById('output').innerHTML = myJson['Value'];
   });
+
+fetch(arc_sq_ft_key)
+.then((response) => {
+  return response.json();
+})
+.then((myJson) => {
+  console.log(myJson['Value']);
+  document.getElementById('sqft').innerHTML = myJson['Value'];
+});
+
